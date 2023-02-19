b7680a53a679b1be0cee8141fbc170ab3b50c958
ManavJoshi <manavjoshi154@gmail.com>
2023-02-19 20:18:26 +0530

    Final Changes

--- index.js
+++ index.js
@@ -10,15 +10,15 @@ const client = new Client({
     GatewayIntentBits.MessageContent,
   ],
 });
-const userOffset = new Date().getTimezoneOffset()*60*1000;
+const userOffset = new Date().getTimezoneOffset() * 60 * 1000;
 const dateToTime = date => date.toLocaleString('en-US', {
-    hour: 'numeric',
-    minute: 'numeric'
-  });
+  hour: 'numeric',
+  minute: 'numeric'
+});
 
 const dotenv = require('dotenv');
 dotenv.config();
-var http = require('http'); http.createServer(function(req, res) { res.write("Bot is running now"); res.end(); }).listen(8080);
+var http = require('http'); http.createServer(function (req, res) { res.write("Bot is running now"); res.end(); }).listen(8080);
 
 const getContest = async () => {
   const data = await fetch("https://kontests.net/api/v1/all");
@@ -30,8 +30,8 @@ const sendReminder = async () => {
   const contests = await getContest();
   for (var contest in contests) {
     if (contests[contest].in_24_hours == "Yes") {
-      let dateString=contests[contest].start_time;
-      let localDate=new Date(dateString);
+      let dateString = contests[contest].start_time;
+      let localDate = new Date(dateString);
       var message = `New Contest Details : \nContest Name : ${contests[contest].name}\nPlatform : ${contests[contest].site}\nDate : ${localDate.getDate()} ${localDate.getMonth()} ${localDate.getFullYear()}\nTime : ${dateToTime(localDate)}\nLink : ${contests[contest].url}`;
 
       client.channels.cache.find(channel => channel.name === 'general').send(message);
@@ -40,7 +40,7 @@ const sendReminder = async () => {
 }
 client.on("ready", () => {
   console.log("I'm ready and logged in as ", client.user.tag);
-  
+
   setInterval(() => {
     sendReminder();
   }, 1000 * 60 * 60 * 2);
@@ -55,10 +55,10 @@ client.on("messageCreate", async msg => {
     const contests = await getContest();
     for (var contest in contests) {
       if (contests[contest].in_24_hours == "Yes") {
-        let dateString=contests[contest].start_time;
-        let localDate=new Date(dateString);
-        let utcDate=localDate.getTime()+userOffset;
-        
+        let dateString = contests[contest].start_time;
+        let localDate = new Date(dateString);
+        let utcDate = localDate.getTime() + userOffset;
+
         console.log("In 24 hours : ", contests[contest]);
         var message = `${contests[contest].site} , is having contest named : ,${contests[contest].name}, at : ,${contests[contest].start_time}`;
         msg.channel.send(message);
